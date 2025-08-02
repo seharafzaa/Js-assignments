291f82db29c7ad63e5848f39708cee693141059b
Sehar AbdulGhani <seharafzaa96@gmail.com>
2025-08-02 16:48:07 +0500

    Add files via upload

---
+++ chp no 5/app.js
@@ -0,0 +1,37 @@
+// Question no 1 //
+
+let remainder = 10 % 3; // remainder is 1 and the name of this symbol is modulus
+ console.log(remainder);
+
+// Question no 2 //
+
+var num = 20 % 6;    //the value of num is 2
+console.log(num)
+
+//Question no 3 //
+
+var largeNum = 1000 * 2000;
+console.log(largeNum)
+
+//Question no 4 //
+
+var  variable1 = 10;
+var  variable2 = 25;
+
+var result = variable2 - variable1;
+console.log(result); 
+
+//Question no 5 //
+
+var  remainderResult = 45 % 8;
+
+
+console.log(remainderResult); // This will print 5
+
+//Question no 6 //
+
+var  num1 = 5;
+var  num2 = 10;
+var  product = num1 * num2;
+
+alert("The result of multiplication is: " + product);
