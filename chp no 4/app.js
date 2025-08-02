367dd672cbd00978c328dae75a638dbd4dc51a80
Sehar AbdulGhani <seharafzaa96@gmail.com>
2025-08-02 16:45:41 +0500

    Add files via upload

---
+++ chp no 4/app.js
@@ -0,0 +1,33 @@
+// Question no 1 //
+
+var productcost=3.45;
+console.log(productcost)
+
+// Question no 2 //
+
+var NameOfband=1; 
+console.log(NameOfband)
+
+  // Question no 3 //
+
+  let myVariable = 10;
+
+//   Question no 4 //
+
+let firstNameLastName = "JohnDoe";
+
+// Question no 5 //
+
+// Legal names and illegal names
+
+// let myVariable = 10;
+// let _myVar = 20;
+// let var123 = 30;
+// let name = "Alice";
+
+// illegal names 
+
+// let 1variable = 10;  
+// let my-variable = 20;
+// let @variable = 30;    
+// let let = 40;          
